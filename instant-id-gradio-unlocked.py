6ed7cfd71070e80b326f517efba1df9107521c32
eniora <116663473+eniora@users.noreply.github.com>
2025-07-09 17:26:54 +0300

    Added triple Lora loader. Finally after a lot of work

--- instant-id-gradio-unlocked.py
+++ instant-id-gradio-unlocked.py
@@ -1149,7 +1149,7 @@ Scheduler: {scheduler}"""
                 accordion_update = gr.update(open=False)
                 settings = {
                     "prompt": "",
-                    "negative_prompt": "",
+                    "negative_prompt": DEFAULT_NEGATIVE_PROFILE,
                     "resize_max_side": 1280,
                     "seed": 42,
                     "num_steps": 25,
