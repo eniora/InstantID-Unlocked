ec79815ed4ca8a1b420784ab072178ba55827c56
eniora <116663473+eniora@users.noreply.github.com>
2025-09-01 22:15:49 +0300

    small fix to applying model from PNG

--- instant-id-gradio-unlocked.py
+++ instant-id-gradio-unlocked.py
@@ -2040,9 +2040,8 @@ Scheduler: {scheduler}"""
                                 settings["model_name"] = model_name
                             else:
                                 gr.Warning(
-                                    f"Model '{model_name}' used for this image can't be found in your models folder. Please choose another or click 'Generate' to try downloading from huggingface-hub."
+                                    f"Model '{model_name}' used for this image can't be found in your models folder. Falling back to default model."
                                 )
-                                settings["model_name"] = model_name
                         elif line.startswith("Detection size:"):
                             size = line.replace("Detection size:", "").strip()
                             for key, value in DET_SIZE_OPTIONS.items():
