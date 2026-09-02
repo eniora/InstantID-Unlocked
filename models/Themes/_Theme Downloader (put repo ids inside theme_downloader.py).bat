ef4a21e515757577f85cffa8c823f48a3b7e18a6
eniora <116663473+eniora@users.noreply.github.com>
2026-09-02 17:37:12 +0300

    Added Themes folder with a themes downloader script

---
+++ models/Themes/_Theme Downloader (put repo ids inside theme_downloader.py).bat
@@ -0,0 +1,16 @@
+@echo off
+cd /d "%~dp0"
+if exist "venv\Scripts\activate.bat" (
+    call venv\Scripts\activate.bat
+) else if exist "..\venv\Scripts\activate.bat" (
+    call ..\venv\Scripts\activate.bat
+) else if exist "..\..\venv\Scripts\activate.bat" (
+    call ..\..\venv\Scripts\activate.bat
+) else (
+    echo Could not find venv folder.
+    pause
+    exit /b 1
+)
+python _Theme_downloader.py
+echo Downloaded Successfully. Restart InstantID completely for the theme(s) to appear in the dropdown.
+pause
